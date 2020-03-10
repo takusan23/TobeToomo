08a6ad19766862cb64573a48442a0bab0b1c0f14
takusan23 <takumu1200@gmail.com>
2020-03-11 00:06:10 +0900

    とりあえず完成

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ export function main(param: GameMainParameterObject): void {
 	const scene = new g.Scene({
 		game: g.game,
 		// このシーンで利用するアセットのIDを列挙し、シーンに通知します
-		assetIds: ["inu", "n_kou", "result", "intai"]
+		assetIds: ["intai", "inu", "kanemoti", "karaoke_2", "karaoke", "kiyomizu", "korean", "launch", "n_kou", "result",
+			// tslint:disable-next-line: max-line-length
+			"doumo_toomo", "gogo_no_zyugyou", "hattastu_syougai", "karaoke_ikuka", "katsudon_channel", "korean_sound", "n_kou_taigaku", "san_ryuunen", "teacher_block"]
 	});
-	let time = 70; // 制限時間
+	let time = 60; // 制限時間
 	if (param.sessionParameter.totalTimeLimit) {
 		time = param.sessionParameter.totalTimeLimit; // セッションパラメータで制限時間が指定されたらその値を使用します
 	}
@@ -21,18 +23,21 @@ export function main(param: GameMainParameterObject): void {
 		assetIds: ["title"]
 	});
 	titleScene.loaded.add(() => {
-		// タイトル画像
+		// 読み込み成功
+		// タイトル画面出す
 		const titleSprite = new g.Sprite({
 			scene: titleScene,
 			src: titleScene.assets["title"]
 		});
-		// タイトル画像追加
+		// 追加
 		titleScene.append(titleSprite);
-		// 読み込み成功時
+		// 5秒待つ
 		titleScene.setTimeout(() => {
-			// 5秒経ったらゲーム開始。
-			time -= 5;
 			scene.loaded.add(() => {
+
+				// 5秒引く
+				time -= 5;
+
 				// ここからゲーム内容を記述します
 
 				// 背景用意
@@ -64,7 +69,7 @@ export function main(param: GameMainParameterObject): void {
 				// 残り時間表示用ラベル
 				const timeLabel = new g.Label({
 					scene: scene,
-					text: "時間: 70",
+					text: "残り時間: 0",
 					font: font,
 					fontSize: font.size / 2,
 					textColor: "black",
@@ -72,13 +77,14 @@ export function main(param: GameMainParameterObject): void {
 				});
 				scene.append(timeLabel);
 
-				// プレイヤーの表示
+				// プレイヤー画像
 				const playerSprite = new g.Sprite({
 					scene: scene,
-					src: scene.assets["inu"],
-					y: 200,
-					x: 100
+					src: scene.assets["inu"]
 				});
+				playerSprite.x = 100;
+				playerSprite.y = 200;
+				playerSprite.modified();
 				scene.append(playerSprite);
 
 				// 地面
@@ -86,24 +92,31 @@ export function main(param: GameMainParameterObject): void {
 					scene: scene,
 					width: g.game.width,
 					height: 20,
-					cssColor: "black",
-					y: (playerSprite.y + playerSprite.height) // プレイヤーの下にいるように
+					cssColor: "black"
 				});
+				groundFilledRect.y = playerSprite.y + playerSprite.height; // 地面の高さ
+				groundFilledRect.modified();
 				scene.append(groundFilledRect);
 
-				// 物理の授業。
-				// 押したらジャンプできるようにする。
+				// 音の名前の配列
+				const soundList = ["doumo_toomo", "gogo_no_zyugyou", "hattastu_syougai", "karaoke_ikuka", "katsudon_channel", "korean_sound", "n_kou_taigaku", "san_ryuunen", "teacher_block"];
+
+				// ジャンプする
 				// 連打対策
-				let isJump = true; // ジャンプ可能ならtrue
+				let isJump = true;
 				scene.pointDownCapture.add(() => {
 					if (isJump) {
+						// 音発生！
+						const soundRandom = g.game.random.get(0, soundList.length - 1);
+						(scene.assets[soundList[soundRandom]] as g.AudioAsset).play();
+						// 押せなくする
 						isJump = false;
-						// 計算に必要なもの
-						const v0 = 15; // 初速度
-						const gravity = 0.9; // 重力加速度
-						const ground = 210; // 地面の位置
-						let jumpTime = 0; // 時間
-						// 毎フレーム呼ばれる関数
+						// 計算に必要な奴
+						const v0 = 15;
+						const gravity = 0.9;
+						const ground = 210;
+						let jumpTime = 0;
+						// 計算する
 						const jumpUpdateFanc = () => {
 							// 公式
 							const calc = (0.5 * gravity * jumpTime * jumpTime - v0 * jumpTime + ground);
@@ -111,87 +124,63 @@ export function main(param: GameMainParameterObject): void {
 								// ジャンプ中
 								playerSprite.y = calc;
 							} else {
-								// 着地
-								playerSprite.update.remove(jumpUpdateFanc); // 毎フレーム呼ばれる関数解除
-								isJump = true; // ジャンプできるようにする
+								playerSprite.update.remove(jumpUpdateFanc);
+								isJump = true;
 							}
-							jumpTime += 1;
+							jumpTime++;
 							playerSprite.modified();
 						};
-						playerSprite.update.add(jumpUpdateFanc); // 毎フレーム呼ばれる関数登録
+						playerSprite.update.add(jumpUpdateFanc);
 					}
 				});
 
-				// 障害物作成
+				// 障害物？作成
+				// 使う画像の名前の配列
+				const imgList = ["intai", "kanemoti", "karaoke_2", "karaoke", "kiyomizu", "korean", "launch", "n_kou"];
+				// 定期実行
 				scene.setInterval(() => {
-					if (time >= 5) {
-						// 時間内
-						// ちょっとランダム
-						const timeRandom = g.game.random.get(500, 1000);
-						scene.setTimeout(() => {
-							const nKouSprite = new g.Sprite({
-								scene: scene,
-								src: scene.assets["n_kou"]
-							});
-							// 位置
-							nKouSprite.y = groundFilledRect.y - nKouSprite.height;
-							nKouSprite.x = g.game.width + nKouSprite.width;
-							// 動かす
-							nKouSprite.update.add(() => {
-								if (time >= 5) {
-									nKouSprite.x -= 10;
-									nKouSprite.modified();
-									// 当たり判定
-									if (g.Collision.intersectAreas(nKouSprite, playerSprite)) {
-										// 当たったら消して減点！
-										nKouSprite.destroy();
-										g.game.vars.gameState.score -= 50;
-										scoreLabel.text = `スコア: ${g.game.vars.gameState.score}`;
-										scoreLabel.invalidate(); // 再描画
-									}
+					// 生成も少しずらす
+					const timeoutRandom = g.game.random.get(500, 1000);
+					scene.setTimeout(() => {
+						// 画像はらんだむ
+						const random = g.game.random.get(0, imgList.length - 1);
+						const obj = new g.Sprite({
+							scene: scene,
+							src: scene.assets[imgList[random]]
+						});
+						// いち
+						obj.x = g.game.width + obj.width;
+						obj.y = groundFilledRect.y - obj.height;
+						// 動かす
+						obj.update.add(() => {
+							// 時間内なら
+							if (time >= 5) {
+								obj.x -= 10;
+								obj.modified();
+								// 当たったら
+								if (g.Collision.intersectAreas(obj, playerSprite)) {
+									// 消す＋減点
+									obj.destroy();
+									g.game.vars.gameState.score -= 50;
+									// テキスト反映
+									scoreLabel.text = `スコア: ${g.game.vars.gameState.score}`;
+									scoreLabel.invalidate();
 								}
-							});
-							scene.append(nKouSprite);
-						}, timeRandom);
-					}
+							}
+						});
+						scene.append(obj);
+					}, timeoutRandom);
 				}, 1500);
 
-				// // おまけ。得点要素も
-				// scene.setInterval(() => {
-				// 	// ランダムで出すか決定
-				// 	const random = g.game.random.get(1, 5);
-				// 	if (random === 5) {
-				// 		const intaiSprite = new g.Sprite({
-				// 			scene: scene,
-				// 			src: scene.assets["intai"]
-				// 		});
-				// 		intaiSprite.x = (g.game.width + intaiSprite.width);
-				// 		intaiSprite.y = g.game.random.get(50, 200); // 高さはランダム
-				// 		intaiSprite.update.add(() => {
-				// 			if (time >= 5) {
-				// 				intaiSprite.x -= 10;
-				// 				intaiSprite.modified();
-				// 				// 当たり判定
-				// 				if (g.Collision.intersectAreas(intaiSprite, playerSprite)) {
-				// 					// 当たったら消して点数入れる！
-				// 					intaiSprite.destroy();
-				// 					g.game.vars.gameState.score += 500;
-				// 					scoreLabel.text = `スコア: ${g.game.vars.gameState.score}`;
-				// 					scoreLabel.invalidate(); // 再描画
-				// 				}
-				// 			}
-				// 		});
-				// 		scene.append(intaiSprite);
-				// 	}
-				// }, 1000);
-
-				// 進んだら点数を加算。
-				playerSprite.update.add(() => {
-					// スコア
-					g.game.vars.gameState.score += 1;
-					// スコアテキスト反映
-					scoreLabel.text = `スコア: ${g.game.vars.gameState.score}`;
-					scoreLabel.invalidate(); // 再描画
+				// 進んだら点数あっぷ
+				scene.update.add(() => {
+					// 時間内なら
+					if (time >= 5) {
+						g.game.vars.gameState.score++;
+						// テキスト反映
+						scoreLabel.text = `スコア: ${g.game.vars.gameState.score}`;
+						scoreLabel.invalidate();
+					}
 				});
 
 				const updateHandler = () => {
@@ -205,21 +194,17 @@ export function main(param: GameMainParameterObject): void {
 						}
 						scene.update.remove(updateHandler); // カウントダウンを止めるためにこのイベントハンドラを削除します
 					}
-					// ゲーム終了
+					// 終了画面
 					if (time <= 5) {
-						// 終了画面
 						const resultSprite = new g.Sprite({
 							scene: scene,
 							src: scene.assets["result"]
 						});
 						scene.append(resultSprite);
-						// 押せなくする
-						scene.pointDownCapture.removeAll();
-						playerSprite.update.removeAll();
 					}
 					// カウントダウン処理
 					time -= 1 / g.game.fps;
-					timeLabel.text = "残り時間: " + Math.ceil(time - 5) + " 秒"; // 最後の5秒は終了画面のために使う。
+					timeLabel.text = "残り時間: " + Math.ceil(time - 5) + " 秒";
 					timeLabel.invalidate();
 				};
 				scene.update.add(updateHandler);
@@ -227,9 +212,8 @@ export function main(param: GameMainParameterObject): void {
 			});
 			g.game.pushScene(scene);
 		}, 5000);
-
 	});
-	// 画面切り替え
+	// シーン切り替え
 	g.game.pushScene(titleScene);
 
 }
